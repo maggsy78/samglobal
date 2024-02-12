717198acb5a77a2e260343cae9a44573aa4d1ed5
Steve Maggs <steve.maggs@iprospect.com>
2024-02-12 08:55:35 +0000

    Meta OG data added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
         { hid: 'og:url', property: 'og:url', content: 'https://samglobal.agency' },
         { hid: 'og:description', property: 'og:description', content: 'SAM Global is the face of Sherwood Artist Management. Management and PR for the hottest talent and artists.' },
         { hid: 'og:image', property: 'og:image', content: 'https://opengraph.b-cdn.net/production/documents/df131206-d634-4c49-8257-3b90188346b1.png?token=Av6RC5QzhvtpTpCxZT5iGc2jeb1WF6MuHfumoArWHhw&height=630&width=1200&expires=33243726844' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
         
         // twitter card
         { hid: 'twitter:title', name: 'twitter:title', content: 'The home of SAM Global - Sherwood Artist Management' },
